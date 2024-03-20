75e0a03107a9fee24ca2d01f140709c4eda63da2
Manjula88 <manju.ramanathan@gmail.com>
2024-03-20 18:57:31 -0400

    add updated js  files

--- page-objects/parabank.js
+++ page-objects/parabank.js
@@ -4,6 +4,7 @@ const helpers = require("../runtime/helpers");
 
 /* eslint-disable no-undef */
 module.exports = {
+
     parabank: 'https://parabank.parasoft.com/parabank/index.htm',
 
 
@@ -30,16 +31,16 @@ module.exports = {
         loginButton: '//*[@id="loginPanel"]/form/div[3]/input',
         CustomerPage: '//*[@id="rightPanel"]/div/div/h1',
 
-        forgotloginButton:'//*[@id="loginPanel"]/p[1]/a',
+        forgotloginButton: '//*[@id="loginPanel"]/p[1]/a',
         CustomerLookupHeader: '//*[@id="rightPanel"]/h1',
-        flfirstName :by.name('firstName'),
+        flfirstName: by.name('firstName'),
         fllastName: by.name('lastName'),
         fladdress: by.name('address.street'),
         flcity: by.name('address.city'),
         flstate: by.name('address.state'),
         flzipCode: by.name('address.zipCode'),
         flssnNumber: by.name('ssn'),
-        findLoginInfoButton:'//*[@id="lookupForm"]/table/tbody/tr[8]/td[2]/input',
+        findLoginInfoButton: '//*[@id="lookupForm"]/table/tbody/tr[8]/td[2]/input',
         loginInfoDetails: '//*[@id="rightPanel"]/p[1]',
 
 
@@ -47,8 +48,8 @@ module.exports = {
         customerCarePage: '//*[@id="rightPanel"]/h1',
         ccName: by.name('name'),
         ccEmail: by.name('email'),
-        ccPhone:by.name('phone'),
-        ccMessage:by.name('message'),
+        ccPhone: by.name('phone'),
+        ccMessage: by.name('message'),
         sendToCuscareButton: '//*[@id="contactForm"]/table/tbody/tr[5]/td[2]/input',
         thankYouRepPage: '//*[@id="rightPanel"]/p[1]',
         customerCareButton: '//*[@id="headerPanel"]/ul[2]/li[3]/a',
@@ -62,13 +63,70 @@ module.exports = {
 
         productsButton: '//*[@id="headerPanel"]/ul[1]/li[4]/a',
         parasoftPage: '//*[@id="main"]/section[2]/div[2]/div/div/div[1]',
-        
+
         locationButton: '//*[@id="headerPanel"]/ul[1]/li[5]/a',
         paraLocationPage: '//*[@id="main"]/section[2]/div[2]/div/div/div',
 
         adminPageButton: '//*[@id="headerPanel"]/ul[1]/li[6]/a',
         paraAdminPage: '//*[@id="rightPanel"]/h1',
 
+
+        transferFunds: '//*[@id="leftPanel"]/ul/li[3]/a',
+        fundAmount: by.name('input'),
+        '17895Account': '//*[@id="fromAccountId"]/option[2]',
+        transferButton: '//*[@id="rightPanel"]/div/div/form/div[2]/input',
+        transferCompletePage: '//*[@id="rightPanel"]/div/div/h1',
+
+        openNewAccount: '//*[@id="leftPanel"]/ul/li[1]/a',
+        savingsAccOption: '//*[@id="type"]/option[2]',
+        createNewAcc: '//*[@id="rightPanel"]/div/div/form/div/input',
+        accountOpenedPage: '//*[@id="rightPanel"]/div/div/p[1]',
+
+        accOverviewButton: '//*[@id="leftPanel"]/ul/li[2]/a',
+        accountOverviewPage: '//*[@id="rightPanel"]/div/div/h1',
+        accdetailsof27330: '//*[@id="accountTable"]/tbody/tr[1]/td[1]/a',
+        accountDetailsPage: '//*[@id="rightPanel"]/div/div[1]/h1',
+        activityPeriodOption: '//*[@id="month"]/option[4]',
+        activityType: '//*[@id="transactionType"]/option[2]',
+        goButton: '//*[@id="rightPanel"]/div/div[2]/form/table/tbody/tr[3]/td[2]/input',
+        transactionDetailsList: '//*[@id="transactionTable"]',
+        ftReceivedButton: '//*[@id="transactionTable"]/tbody/tr[1]/td[2]/a',
+        transactionDetailsPage: '//*[@id="rightPanel"]/h1',
+
+        billPayButton: '//*[@id="leftPanel"]/ul/li[4]/a',
+        billPaymentServicePage: '//*[@id="rightPanel"]/div/div[1]/h1',
+        payeeName: by.name('payee.name'),
+        payeeAddress: by.name('payee.address.street'),
+        payeeCity: by.name('payee.address.city'),
+        payeeState: by.name('payee.address.state'),
+        payeeZipcode: by.name('payee.address.zipCode'),
+        payeePhone: by.name('payee.phoneNumber'),
+        payeeAccount: by.name('payee.accountNumber'),
+        payeeVerifyAccount: by.name('verifyAccount'),
+        payeeAmount: by.name('amount'),
+        senPaymentButton: '//*[@id="rightPanel"]/div/div[1]/form/table/tbody/tr[14]/td[2]/input',
+        billPayCompletePage: '//*[@id="rightPanel"]/div/div[2]/h1',
+
+
+
+        updateInfoButton: '//*[@id="leftPanel"]/ul/li[6]/a',
+        updateProfilePage: '//*[@id="rightPanel"]/div/div/h1',
+        profileFirstName: by.name('customer.firstName'),
+        profileLastName: by.name('customer.lastName'),
+        profileAddress: by.name('customer.address.street'),
+        profileCity: by.name('customer.address.city'),
+        profileState: by.name('customer.address.state'),
+        profileZipcode: by.name('customer.address.zipcode'),
+        profilePhone: by.name('customer.phoneNumber'),
+        updateProfileButton: '//*[@id="rightPanel"]/div/div/form/table/tbody/tr[8]/td[2]/input',
+        profileUpdatedPage: '//*[@id="rightPanel"]/div/div/h1',
+
+        requestLoanButton: '//*[@id="leftPanel"]/ul/li[7]/a',
+        applyLoanPage: '//*[@id="rightPanel"]/div/div/h1',
+        loanAmount: by.id('amount'),
+        downPayment: by.id('downPayment'),
+        applyNowButton: '//*[@id="rightPanel"]/div/div/form/table/tbody/tr[4]/td[2]/input',
+        loanApprovedPage: '//*[@id="loanProviderName"]',
 
     },
     clickElement: async function (objectKey) {
@@ -78,11 +136,13 @@ module.exports = {
     },
 
     inputElement: async function (inputname, inputvalue) {
+        console.log('inputname= ' + inputname)
+        console.log('inputvalue= ' + inputvalue)
         const selector = this.elements[inputname];
+
+        console.log('inputname= ' + inputname)
         await driver.sleep(3000);
         return driver.findElement(selector).sendKeys(inputvalue);
-
-
     },
 
     elementExists: async function (objectKey) {
--- page-objects/swaglabs.js
+++ page-objects/swaglabs.js
@@ -10,22 +10,63 @@ module.exports = {
     elements: {
         username: by.name('user-name'),
         password: by.name('password'),
-        loginButton: '//*[@id="login-button"]',
-        swagLabsPage: '//*[@id="header_container"]/div[1]/div[2]/div',
+        loginBttn: '//*[@id="login-button"]',
+
+        saucelabsBackPack: '//*[@id="add-to-cart-sauce-labs-backpack"]',
+        saucelabsTshirt: '//*[@id="add-to-cart-sauce-labs-bolt-t-shirt"]',
+        saucelabsBikeLight: '//*[@id="add-to-cart-sauce-labs-bike-light"]',
+        saucelabsJacket: '//*[@id="add-to-cart-sauce-labs-fleece-jacket"]',
+        cartbutton: '//*[@id="shopping_cart_container"]/a',
+        yourcart: '//*[@id="header_container"]/div[2]/span',
+       
+        checkoutButton: '//*[@id="checkout"]',
+        firstName: by.name('firstName'),
+        lastName: by.name('lastName'),
+        zipcode: by.name('postalCode'),
+        continueButton: '//*[@id="continue"]',
+        finishButton: '//*[@id="finish"]' ,
+        thankyouForOrderPage: '//*[@id="checkout_complete_container"]/h2',
+
+
+        filterNameZtoA: '//*[@id="header_container"]/div[2]/div/span/select/option[2]',
+        allTheThingsTshirt: '//*[@id="item_3_title_link"]/div',
+        addtocart: '//*[@id="add-to-cart-test.allthethings()-t-shirt-(red)"]',
+
+
+        nameAtoZ: '//*[@id="header_container"]/div[2]/div/span/select/option[1]',
+        nameZtoA: '//*[@id="header_container"]/div[2]/div/span/select/option[2]',
+        priceHightoLow: '//*[@id="header_container"]/div[2]/div/span/select/option[3]',
+        priceLowtoHigh: '//*[@id="header_container"]/div[2]/div/span/select/option[4]',
+        allProducts: '//*[@id="header_container"]/div[2]/span',
+
+        removeSaucelabsTshirt: '//*[@id="remove-sauce-labs-bolt-t-shirt"]',
+        removeSaucelabsBikeLight: '//*[@id="remove-sauce-labs-bike-light"]',
+        continueShopping: '//*[@id="continue-shopping"]',
+
+        allitemsMenuButton: '//*[@id="react-burger-menu-btn"]',
+        logoutButton: '//*[@id="logout_sidebar_link"]',
+        loginPage: '//*[@id="root"]/div/div[1]'
+
+
     },
+
     clickElement: async function (objectKey) {
-        const selector = this.elements[objectKey];
+        console.log('objectkey= ' + objectKey)
+
+        const selector = page.swaglabs.elements[objectKey];
+        console.log('selector= ' + selector)
+
         await driver.sleep(3000);
         return driver.findElement(By.xpath(selector)).click();
+
     },
 
     inputElement: async function (inputname, inputvalue) {
         const selector = this.elements[inputname];
         await driver.sleep(3000);
         return driver.findElement(selector).sendKeys(inputvalue);
-
-
     },
+
 
     elementExists: async function (objectKey) {
         const selector = this.elements[objectKey];
