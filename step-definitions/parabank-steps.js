4bfae18c7f1de7a8f3b9a26ce06beabbc8954457
Manjula88 <manju.ramanathan@gmail.com>
2024-03-20 18:58:11 -0400

    add updated step def  files

--- step-definitions/parabank-steps.js
+++ step-definitions/parabank-steps.js
@@ -3,27 +3,50 @@ const helpers = require("../runtime/helpers");
 module.exports = function () {
 
     this.Given(/^I am on "([^"]*)" websitepage$/, function (objectKey) {
-        return helpers.loadPage(page.parabank[objectKey],2000);
+        return helpers.loadPage(page.parabank[objectKey], 2000);
     });
 
-    this.And(/^I click on "([^"]*)"$/, function(objectKey) { 
-        return page.parabank.clickElement(objectKey);
-    });
+
 
     this.Then(/^I should see "([^"]*)"$/, function (objectKey) {
         return page.parabank.elementExists(objectKey);
     });
 
-    this.When(/^I enter the inputs details$/, function(dataTable) { 
+
+    this.Then(/^I see "([^"]*)"$/, function (objectKey) {
+        return page.parabank.elementExists(objectKey);
+    });
+
+
+    this.When(/^I enter the inputs details$/, function (dataTable) {
         const rows = dataTable.rows()
-        for(var i=0; i<rows.length; i++){
+        for (var i = 0; i < rows.length; i++) {
             var inputname = rows[i][0]
             var inputvalue = rows[i][1]
             page.parabank.inputElement(inputname, inputvalue)
         }
-        return ;
+        return;
     });
 
-   
+
+    this.When(/^I am logging into the parabank website as "([^"]*)" and "([^"]*)"$/, function (cusUserName, cusPassword) {
+        helpers.loadPage(page.parabank.parabank);
+        page.parabank.inputElement('cusUserName', cusUserName);
+        page.parabank.inputElement('cusPassword', cusPassword);
+        page.parabank.clickElement('loginButton');
+        //page.parabank.elementExists('CustomerPage');
+        return
+    });
+
+
+    this.When(/^I click on "([^"]*)"$/, function (objectKey) {
+        return page.parabank.clickElement(objectKey);
+    });
+
+
+    this.When(/^I enter the "([^"]*)" as "([^"]*)"$/, function (inputname, inputvalue) {
+        return page.parabank.inputElement(inputname, inputvalue)
+    });
+
 
 }
--- step-definitions/swaglabs-steps.js
+++ step-definitions/swaglabs-steps.js
@@ -12,10 +12,21 @@ module.exports = function () {
     });
 
   
-
-    this.And(/^I click on the "([^"]*)"$/, function(objectKey) { 
+    this.When(/^I click on the "([^"]*)"$/, function(objectKey) { 
         return page.swaglabs.clickElement(objectKey);
     });
+
+
+    this.When(/^I click on the saucebuttons$/, function (dataTable){
+        const rows = dataTable.rows()
+        for (var i = 0; i < rows.length; i++) {
+            var filtername = rows[i][0]
+            page.swaglabs.clickElement(filtername);
+        }
+        return;
+
+    });
+
 
     this.Then(/^I should see the "([^"]*)"$/, function (objectKey) {
         return page.swaglabs.elementExists(objectKey);
@@ -23,6 +34,26 @@ module.exports = function () {
 
 
 
+    this.When(/^I am logging into the swaglabs website as "([^"]*)" and "([^"]*)"$/, function(username, password) { 
+        helpers.loadPage(page.swaglabs.swaglabs);
+        page.swaglabs.inputElement('username', username);
+        page.swaglabs.inputElement('password', password);
+        page.swaglabs.clickElement('loginBttn');
+        return 
+    });
    
+
+
+    this.When(/^I enter the customer details$/, function (dataTable) {
+        const rows = dataTable.rows()
+        for (var i = 0; i < rows.length; i++) {
+            var inputname = rows[i][0]
+            var inputvalue = rows[i][1]
+            page.swaglabs.inputElement(inputname, inputvalue)
+        }
+        return;
+    });
+
+
 
 }
